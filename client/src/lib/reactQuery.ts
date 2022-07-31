a673439cb681723425a38d0ec3fe3a8593d8ee9b
mhshifat <mehedihassanshifat01@gmail.com>
2022-07-31 20:49:09 +0600

    updated readme

--- client/src/lib/reactQuery.ts
+++ client/src/lib/reactQuery.ts
@@ -9,6 +9,7 @@ export const queryClient = new QueryClient({
       refetchOnMount: false,
       refetchOnReconnect: false,
       refetchOnWindowFocus: false,
+      retry: false
     },
   },
 });
